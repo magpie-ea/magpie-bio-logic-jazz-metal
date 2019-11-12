4b6bf8fda4337a79611abf4198b170243f391b36
MalinSpaniol <mspaniol@uos.de>
2019-11-12 13:17:32 +0100

    mini experiment forced choice

---
+++ 04_trials.js
@@ -0,0 +1,22 @@
+// In this file you can specify the trial data for your experiment
+
+
+const trial_info = {
+  forced_choice: [
+    {
+      question: "If you have to choose between the following two options, which one do you prefer?",
+      option1: 'Biology',
+      option2: 'Logic'
+        },
+    {
+      question: "If you have to choose between the following two options, which one do you prefer?",
+      option1: "Metal",
+      option2: "Jazz"
+        },
+    {
+      question: "If you have to choose between the following two options, which one do you prefer?",
+      option1: "Mountains",
+      option2: "Beach"
+        }
+    ]
+};
